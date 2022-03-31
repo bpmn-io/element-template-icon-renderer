25dbb5eabecccbca7c78256285cb3b8f63bf4562
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-03-31 12:00:01 +0200

    feat: initial renderer
    Closes #1

---
+++ src/ElementTemplatesIconsRenderer.js
@@ -0,0 +1,77 @@
+import inherits from 'inherits';
+
+import BaseRenderer from 'diagram-js/lib/draw/BaseRenderer';
+
+import {
+  is
+} from 'bpmn-js/lib/util/ModelUtil';
+
+import {
+  isLabel
+} from 'bpmn-js/lib/util/LabelUtil';
+
+import {
+  append as svgAppend,
+  attr as svgAttr,
+  create as svgCreate
+} from 'tiny-svg';
+
+import {
+  getElementTemplate,
+  getModelerTemplateIcon
+} from './util';
+
+var HIGH_PRIORITY = 1250;
+
+
+export default function IconsRenderer(
+    bpmnRenderer,
+    config,
+    eventBus
+) {
+  this._bpmnRenderer = bpmnRenderer;
+  this._config = config || {};
+
+  BaseRenderer.call(this, eventBus, HIGH_PRIORITY);
+}
+
+inherits(IconsRenderer, BaseRenderer);
+
+IconsRenderer.prototype.canRender = function(element) {
+
+  if (isLabel(element)) {
+    return false;
+  }
+
+  return (
+    is(element, 'bpmn:Task') &&
+    !!getElementTemplate(element) &&
+    !!getModelerTemplateIcon(element)
+  );
+};
+
+IconsRenderer.prototype.drawShape = function(parentGfx, element) {
+
+  var renderer = this._bpmnRenderer.handlers['bpmn:Task'];
+
+  var gfx = renderer(parentGfx, element);
+
+  var modelerTemplateIcon = getModelerTemplateIcon(element);
+
+  var icon = svgCreate('image');
+  svgAttr(icon, {
+    href: modelerTemplateIcon,
+    x: 4,
+    y: 4
+  });
+
+  svgAppend(parentGfx, icon);
+
+  return gfx;
+};
+
+IconsRenderer.$inject = [
+  'bpmnRenderer',
+  'config.bpmnRenderer',
+  'eventBus',
+];
