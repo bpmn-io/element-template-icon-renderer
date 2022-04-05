ed1a604a729d2be08bb05659817f090b960e65ce
Martin Stamm <martin.stamm@camunda.com>
2022-04-05 14:53:40 +0200

    fix: move `modelerTemplateIcon` to property

--- src/util/index.js
+++ src/util/index.js
@@ -1,31 +1,8 @@
 import {
-  getBusinessObject,
-  is
+  getBusinessObject
 } from 'bpmn-js/lib/util/ModelUtil';
 
 export function getModelerTemplateIcon(element) {
-  var modelerTemplateIcon = findExtension(element, 'zeebe:ModelerTemplateIcon');
-  return modelerTemplateIcon && (modelerTemplateIcon.$body || modelerTemplateIcon.body);
-}
-
-export function findExtension(element, type) {
-  var businessObject = getBusinessObject(element);
-
-  var extensionElements;
-
-  if (is(businessObject, 'bpmn:ExtensionElements')) {
-    extensionElements = businessObject;
-  } else {
-    extensionElements = businessObject.get('extensionElements');
-  }
-
-  if (!extensionElements) {
-    return null;
-  }
-
-  return extensionElements.get('values').find((value) => {
-
-    // make sure we can read the model without moddle descriptors to be required
-    return value && value.$type.toLowerCase() === type.toLowerCase();
-  });
+  var modelerTemplateIcon = getBusinessObject(element).get('zeebe:modelerTemplateIcon');
+  return modelerTemplateIcon;
 }
